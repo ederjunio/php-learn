fb794882c659594fe746787994bcedc9084b1a25
ederjunio <eder.xavier.ti@gmail.com>
2021-04-10 16:41:00 -0300

    erro de permissão corrigido

--- prog8.php
+++ prog8.php
@@ -20,6 +20,20 @@
 	echo 'Mais um teste.....<br>'; 
 	echo 'Mais um teste.....<br>'; 
 	echo 'Mais um teste.....<br>'; 
+
+	echo '<br>';
+
+	echo 'PHP 8 arquivo Prog8.php <br>'; 
+	echo 'Mais um teste.....<br>'; 
+	echo 'Mais um teste.....<br>'; 
+	echo 'Mais um teste.....<br>'; 
+
+	echo '<br>';
+
+	echo 'PHP 8 arquivo Prog8.php<br>'; 
+	echo 'Mais um teste.....<br>'; 
+	echo 'Mais um teste.....<br>'; 
+	echo 'Mais um teste.....<br>'; 
 	
 ?>
 </body>
