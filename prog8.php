dc206b62d60e4430a65915c2ad02d5e31db60def
ederjunio <eder.xavier.ti@gmail.com>
2021-04-10 16:16:36 -0300

    prog 8 teste

--- prog8.php
+++ prog8.php
@@ -1,25 +1,21 @@
 <html>
 <body>
 <?php
-	echo 'PHP 8 arquivo Prog8.php'; <br>
-	echo 'Mais um teste.....'; <br>
-	echo 'Mais um teste.....'; <br>
-	echo 'Mais um teste.....'; <br>
 
-	echo 'PHP 8 arquivo Prog8.php'; <br>
-	echo 'Mais um teste.....'; <br>
-	echo 'Mais um teste.....'; <br>
-	echo 'Mais um teste.....'; <br>
+	echo 'PHP 8 arquivo Prog8.php'<br>;
+	echo 'Mais um teste.....'; 
+	echo 'Mais um teste.....'; 
+	echo 'Mais um teste.....'; 
 
-	echo 'PHP 8 arquivo Prog8.php'; <br>
-	echo 'Mais um teste.....'; <br>
-	echo 'Mais um teste.....'; <br>
-	echo 'Mais um teste.....'; <br>
+	echo 'PHP 8 arquivo Prog8.php'; 
+	echo 'Mais um teste.....'; 
+	echo 'Mais um teste.....'; 
+	echo 'Mais um teste.....'; 
 
-	echo 'PHP 8 arquivo Prog8.php'; <br>
-	echo 'Mais um teste.....'; <br>
-	echo 'Mais um teste.....'; <br>
-	echo 'Mais um teste.....'; <br>
+	echo 'PHP 8 arquivo Prog8.php'; 
+	echo 'Mais um teste.....'; 
+	echo 'Mais um teste.....'; 
+	echo 'Mais um teste.....'; 
 
 ?>
 </body>
